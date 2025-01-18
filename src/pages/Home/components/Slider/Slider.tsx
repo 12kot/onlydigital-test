e84d14c8acc91f8f27e07a6d5a37b263134be69e
Nikitosha <yakol.nikita@gmail.com>
2025-01-18 16:29:42 +0300

    Add swiper + layout

--- src/pages/Home/components/Slider/Slider.tsx
+++ src/pages/Home/components/Slider/Slider.tsx
@@ -1,14 +1,46 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 import { B, P } from 'shared/components';
 
 import styles from './styles.module.scss';
 
+import 'swiper/css';
+
 export const Slider = () => {
-  return <section className={styles.container}><Card /></section>;
+  return (
+    <section className={styles.container}>
+      <Swiper
+        spaceBetween={5}
+        slidesPerView={3}
+      >
+        <SwiperSlide>
+          <Card />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Card />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Card />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Card />
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  );
 };
 
 const Card = () => {
-  return <div className={styles.card}>
-    <B>2015</B>
-    <P>13 сентября — частное солнечное<br />затмение, видимое в Южной<br />Африке и части Антарктиды</P>
-  </div>
-}
+  return (
+    <div className={styles.card}>
+      <B>2015</B>
+      <P>
+        13 сентября — частное солнечное
+        <br />
+        затмение, видимое в Южной
+        <br />
+        Африке и части Антарктиды
+      </P>
+    </div>
+  );
+};
